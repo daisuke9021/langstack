2029c8dfbb7fdbe1c47b410ec31db44a755cf109
daisuke9021 <daisuke.basketball1209@gmail.com>
2021-11-03 21:20:40 +0900

    フロントデザイン修正

--- langstack-front/domains/card/CardServiceImpl.ts
+++ langstack-front/domains/card/CardServiceImpl.ts
@@ -1,11 +1,20 @@
+import axios from "~/infras/AxiosFactory";
 import Card from "@/domains/card/Card";
 import CardService from "@/domains/card/CardService";
+import InitResponse from "@/domains/response/InitResponse";
 
 export default class CardServiceImpl implements CardService {
   addCard(card: Card): void {}
-  getCardsByGenre(genreId: string): Promise<Card[]> {
-    return new Promise<Card[]>(resolve => {
-      resolve([]);
-    });
+  async init(): Promise<InitResponse> {
+    return (await axios.get('/card/init')).data;
+  }
+  async search(keyword: string): Promise<Card[]> {
+    return (await axios.get("/card/search?keyword=" + keyword)).data
+  }
+  async getRecentCards(): Promise<Card[]> {
+    return (await axios.get("/card/recent-cards")).data;
+  }
+  async getCardsByGenre(genreId: string): Promise<Card[]> {
+    return (await axios.get("/card/" + genreId)).data;
   }
 }
